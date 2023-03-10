417c2d7823baf3a487509bb68fa1a6b012f33fe4
DucNguyen <nguyenngocduc94751@gmail.com>
2023-03-10 16:57:22 +0700

    duc upload url

--- src/constants/URLS.js
+++ src/constants/URLS.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:9000";
-// export const API_URL = "https://be-cloud-market-at-home.onrender.com";
+// export const API_URL = "http://localhost:9000";
+export const API_URL = "https://be-cloud-market-at-home.onrender.com";
 // export const API_URL = "http://localhost:80";
